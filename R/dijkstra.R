0a94c7f74a9d1a7ae75346f55a0974a47aec6f5e
arreen <aronens@gmail.com>
2025-09-15 15:43:40 +0200

    Create dijkstra.R

---
+++ R/dijkstra.R
@@ -0,0 +1,14 @@
+#' Add together two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#' @returns The sum of `x` and `y`
+#' @export
+#' @examples
+#' add(1, 1)
+
+
+my_fun <- function(){
+
+  #do nothing
+}
