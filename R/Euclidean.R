11814852e46d594c1286328f962958398d43d45c
arreen <aronens@gmail.com>
2025-09-16 11:37:47 +0200

    Update Euclidean.R

--- R/Euclidean.R
+++ R/Euclidean.R
@@ -1,22 +1,32 @@
 #' Euclidean Algorithm
-#' The Euclidean algorithm finds the common denominator between
+#' The Euclidean algorithm finds the greatest common divisor, GCD between to whole numbers, a and b.
 #'
-#' @param a
+#' @param a a whole number of length 1
+#' @param b a whole number of length 1
 #'
-#' @returns asdfas
+#' @returns The GCD
 #' @export
 #'
-#' @examples print("hi")
+#' @examples
+#' euclidian(123612, 13892347912)
+#' euclidian(100, 1000)
+#'
 #' @source https://en.wikipedia.org/wiki/Euclidean_algorithm
 #'
 euclidean <- function(a, b){
-  if(!is.numeric(a)){stop("Not a numeric value")} # Error message for a
-  if(!is.numeric(b)){stop("Not a numeric value ")} # Error message for b
+  if( !is.numeric(a) | !is.numeric(b) ){stop("Only input numeric values")}
+  if( length(a) != 1 ){stop("a has a length greater than 1")}
+  if( length(b) != 1 ){stop("b has a length greater than 1")}
+  if( (a %% 1 > 0) ){stop("a is not an integer")}
+  if( (b %% 1 > 0) ){stop("b is not an integer")}
+
+
+
   repeat{ # repeat loop since it works best here
     if(a %% b == 0){ # if it can be divided, then stop
       break
     }
-    r <- a %% b # Some smart ass comment about why this is the correct lines
+    r <- a %% b # remainder
     a <- b
     b <- r
   }
