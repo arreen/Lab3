79212f706c761ee592eab6f88939c00d72108844
arreen <aronens@gmail.com>
2025-09-16 11:48:13 +0200

    documentation wiki_graph

--- R/data.R
+++ R/data.R
@@ -1,10 +1,12 @@
-#' title
+#' @title Wiki_graph
+#' @description
+#' The weighted graph from the dijkstra wikipedia page.
 #'
-#' @format A data frame
+#' @format A data frame with 3 columns, v1, v2 and w.
 #' \describe{
-#' text
+#' Each row of the dataframe represents and arc in the graph. From v1 to v2 and its corresponding weight w.
 #'
 #' }
-#' @source wikipedia.com
+#' @source https://en.wikipedia.org/wiki/Dijkstra's_algorithm
 #'
 "wiki_graph"
